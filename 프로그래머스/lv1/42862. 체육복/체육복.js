c948067507c0320183943bfdb6f1d8a81920c1b7
HOSU HAN <tnghgks@naver.com>
2023-04-04 15:10:19 +0900

    [level 1] Title: 체육복, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -0,0 +1,25 @@
+function solution(n, lost, reserve) {
+    
+    lost.sort((a,b) => a-b);
+    reserve.sort((a,b) =>  a-b);            
+    
+    const newLost = lost.filter(item => !reserve.find(element => item === element ))
+    const newReserve = reserve.filter(item => !lost.find(element => item === element ))
+
+    let lostStudent = newLost.filter(lostItem => newReserve.every(resItem => notMatched(lostItem,resItem)))
+             
+    function notMatched(operand,target) {
+        if(operand - 1 === target){
+            newReserve.splice(newReserve.indexOf(target),1)
+            return false
+        }else if (operand + 1 === target){
+            newReserve.splice(newReserve.indexOf(target),1)
+            return false
+        }else {
+            return true
+        }
+}
+    
+    return n - lostStudent.length ;
+}
+
