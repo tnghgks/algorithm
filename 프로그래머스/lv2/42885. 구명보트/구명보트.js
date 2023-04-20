2909c09d2735c3ee799eb861b60d9086a5cedd14
HOSU HAN <tnghgks@naver.com>
2023-04-20 15:16:28 +0900

    [level 2] Title: 구명보트, Time: 12.16 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.js
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.js
@@ -1,4 +1,25 @@
 function solution(people, limit) {
     var answer = 0;
+
+    people.sort((a,b) => a-b)
+
+    let left = 0
+    let right = people.length-1
+
+    while(left <= right){
+        answer += 1;
+        // 혼자남았을때 탈출
+        if(left === right) break;
+        
+        // 남은 Kg이 제일 작은사람을 태울 수 있으면
+        if(limit - people[right] >= people[left]){
+            left += 1
+            right -= 1
+        // 남은 Kg이 제일 작은사람을 태울 수 없으면
+        }else if (limit - people[right] < people[left]){
+            right -= 1
+        }
+    }
+    
     return answer;
 }
