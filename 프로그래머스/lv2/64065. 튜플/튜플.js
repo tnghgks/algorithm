58bf0178db7b51da0296bc7cb83386749cf9d673
HOSU HAN <tnghgks@naver.com>
2023-05-21 17:38:07 +0900

    [level 2] Title: 튜플, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.js
+++ 프로그래머스/lv2/64065. 튜플/튜플.js
@@ -1,25 +1,10 @@
 function solution(s) {
-    // 순회돌아서 중복없는 객체 생성
-    // 순서대로 작은 길이의 원소부터 배열에 넣기
-    var answer = [];
-    const prequency = {};
-    let temp = ""
-    
-    for(let i = 0 ; i < s.length ; i++){
-        if(!isNaN(s[i])){
-            if(!isNaN(s[i+1])){
-                temp += s[i]
-                continue
-            }else{
-                temp += s[i]
-                prequency[temp] = (prequency[temp] || 0) + 1
-                temp = ""
-            }
+    return JSON.parse(s.replace(/{/g, '[').replace(/}/g, ']'))
+    .sort((a, b) => a.length - b.length)
+    .reduce((arr, v, n) => {
+        if (n) {
+            return arr.concat(v.filter(f => !arr.includes(f)));
         }
-    }
-    
-    answer = Object.entries(prequency).sort((a,b)=>  b[1] -a[1]).map(v=> Number(v[0]))
-    
-    
-    return answer;
+        return v;
+    });
 }
