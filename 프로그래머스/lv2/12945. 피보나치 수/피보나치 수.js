b236de5fb37017284b963b5a0d4db7f0a614b66c
HOSU HAN <tnghgks@naver.com>
2023-08-11 16:53:31 +0900

    [level 2] Title: 피보나치 수, Time: 9.74 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
@@ -1,8 +1,9 @@
 function solution(n) {
-    let cache = Array.from({length:n+1},(_,i)=>i)
+    const cache = Array.from({length:n+1},(_,i)=>i)
     
-    for(let i = 2 ; i < cache.length ; i++ ){
-        cache[i] = BigInt(cache[i-1]) + BigInt(cache[i-2])
-    }
-    return Number((cache[n] % 1234567n).toString(10))
+   for(let i = 2 ; i <= n ; i++){
+       cache[i] = (cache[i-1] + cache[i-2]) % 1234567
+   }
+
+   return cache[n]
 }
