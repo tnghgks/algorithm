a958afa3c17318d55a83de69acc4261bdf0d1049
HOSU HAN <tnghgks@naver.com>
2023-08-18 00:05:13 +0900

    [level 2] Title: 예상 대진표, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
@@ -1,9 +1,18 @@
 function solution(n,a,b)
 {
     var answer = 0;
+    let restPlayer = n
+    let aNum = a
+    let bNum = b
 
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    console.log(7/2)
+    while(restPlayer !== 1){
+        answer++
+        if(aNum > bNum && aNum % 2 === 0 && aNum - 1 === bNum) return answer;
+        if(aNum < bNum && bNum % 2 === 0 && bNum - 1 === aNum) return answer;
+        restPlayer = restPlayer/2
+        aNum = Math.ceil(aNum/2)
+        bNum = Math.ceil(bNum/2)
+    }
     
     return answer;
 }
